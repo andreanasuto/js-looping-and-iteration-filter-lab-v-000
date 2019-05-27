2d6fecae38db64b903981c5dbff3694e7069be5f
Andrea Nasuto <andrea.nasuto@gmail.com>
2019-05-27 20:51:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 function findMatching(array, name) {
-  const new_array
+  const new_array = []
   for (const element of array) {
     if (element == name) {
       new_array.push(element)
